9e4b927f174b3992abb8566c2a9bd6c47ce515c1
Tom <tomc@tomvisions.com>
2023-09-25 09:34:07 -0400

    adding files

---
+++ projects/gallery/src/lib/ngx-gallery-arrows/ngx-gallery-arrows.component.ts
@@ -0,0 +1,27 @@
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'ngx-gallery-arrows',
+  templateUrl: './ngx-gallery-arrows.component.html',
+  styleUrls: ['./ngx-gallery-arrows.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NgxGalleryArrowsComponent {
+  @Input() prevDisabled: boolean;
+  @Input() nextDisabled: boolean;
+  @Input() arrowPrevIcon: string;
+  @Input() arrowNextIcon: string;
+
+  @Output() prevClick = new EventEmitter();
+  @Output() nextClick = new EventEmitter();
+
+  constructor() { }
+
+  handlePrevClick(): void {
+    this.prevClick.emit();
+  }
+
+  handleNextClick(): void {
+    this.nextClick.emit();
+  }
+}
